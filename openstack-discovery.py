f5221095a535dc1b9a9fc12c4bc6d8777eabdb5a
Kurt Garloff <kurt@garloff.de>
2022-04-07 09:37:52 +0200

    Use 4-space indentation (PEP8).
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- openstack-discovery.py
+++ openstack-discovery.py
@@ -16,9 +16,9 @@ import yaml
 
 # Global variables
 if "OS_CLOUD" in os.environ:
-        cloud = os.environ["OS_CLOUD"]
+    cloud = os.environ["OS_CLOUD"]
 else:
-        cloud = ""
+    cloud = ""
 conn = None
 ofile = '/dev/stdout'
 
@@ -26,100 +26,102 @@ ofile = '/dev/stdout'
 # to be used by OpenStack and also k8s discovery
 
 class cpu:
-	def __init__(self):
-		# Gaia-X attrs
-		self.cores = 0
-		self.threads = 0
-		self.freq = 0
-		self.boostFreq = 0
-		self.cache = 0
-		# This would not be interesting typically
-		self.socketType = ""
-		# Virt. attrs
-		self.dedicatedCore = false
-		self.dedicatedThread = false
-		self.limitOversubscr = false
+    def __init__(self):
+        # Gaia-X attrs
+        self.cores = 0
+        self.threads = 0
+        self.freq = 0
+        self.boostFreq = 0
+        self.cache = 0
+        # This would not be interesting typically
+        self.socketType = ""
+        # Virt. attrs
+        self.dedicatedCore = false
+        self.dedicatedThread = false
+        self.limitOversubscr = false
 
 class mem:
-	def __init__(self):
-		self.memGB = 0
-		self.ECC = true
+    def __init__(self):
+        self.memGB = 0
+        self.ECC = true
 
 class osService:
-        def __init__(self, dct):
-                self.id = dct["id"]
-                # interface: we only look at public
-                for ep in dct["endpoints"]:
-                        if ep["interface"] != "public":
-                                continue
-                        self.url = ep["url"]
-                        self.region = ep["region_id"]
-                self.type = dct["type"]
-                self.name = dct["name"]
-        def __str__(self):
-                return "%s|%s|%s" % (self.type, self.name, self.url)
-        def __repr__(self):
-                return str(self)
+    def __init__(self, dct):
+        self.id = dct["id"]
+        # interface: we only look at public
+        for ep in dct["endpoints"]:
+            if ep["interface"] != "public":
+                continue
+            self.url = ep["url"]
+            self.region = ep["region_id"]
+        self.type = dct["type"]
+        self.name = dct["name"]
+    def __str__(self):
+        return "%s|%s|%s" % (self.type, self.name, self.url)
+    def __repr__(self):
+        return str(self)
 
 class osCloud:
-        def __init__(self):
-                self.regions = []
-                self.services = []
-        def __str__(self):
-                return "#Regions: %s\n#Services\n%s" % (self.regions, self.services)
-                
+    def __init__(self):
+        self.regions = []
+        self.services = []
+    def __str__(self):
+        return "#Regions: %s\n#Services\n%s" % (self.regions, self.services)
+
 
 def usage(err = 1):
-	print("Usage: openstack-discovery.py [options]", file=sys.stderr)
-	print("You need to have OS_CLOUD set or pass --os-cloud=CLOUD.", file=sys.stderr)
-	sys.exit(err)
+    print("Usage: openstack-discovery.py [options]", file=sys.stderr)
+    print("You need to have OS_CLOUD set or pass --os-cloud=CLOUD.", file=sys.stderr)
+    sys.exit(err)
 
 
 def get_openstack_flavors():
-	for flv_id in conn.compute.flavors(id):
-		flv_name = flv_id['name']
-		flv_cores = flv_id['vcpus']
-		flv_ram = flv_id['ram']
-		for in_count in range(len(flv_id)):
-			data = dict (
-				Name = flv_name,
-				Specs = dict (
-					cores =  flv_cores,
-					memGB =  (flv_ram/1024)
-				)
-			)
-		with open(ofile, 'a') as outfile:
-			yaml.dump(data, outfile, default_flow_style=False)
+    for flv_id in conn.compute.flavors(id):
+        flv_name = flv_id['name']
+        flv_cores = flv_id['vcpus']
+        flv_ram = flv_id['ram']
+        for in_count in range(len(flv_id)):
+            data = dict (
+                Name = flv_name,
+                Specs = dict (
+                    cores =  flv_cores,
+                    memGB =  (flv_ram/1024)
+                )
+            )
+        with open(ofile, 'a') as outfile:
+            yaml.dump(data, outfile, default_flow_style=False)
 
 
 def main(argv):
-        global cloud, conn
-        global ofile
-        try:
-                opts, args = getopt.gnu_getopt(argv[1:], "c:f:h", ("os-cloud=", "file", "help"))
-        except getopt.GetoptError as exc:
-	        usage(1)
-        for opt in opts:
-                if opt[0] == "-h" or opt[0] == "--help":
-                        usage(0)
-                elif opt[0] == "-c" or opt[0] == "--os-cloud":
-                        cloud = opt[1]
-                elif opt[0] == "-f" or opt[0] == "--file":
-                        ofile = opt[1]
+    global cloud, conn
+    global ofile
+    try:
+        opts, args = getopt.gnu_getopt(argv[1:], "c:f:h", ("os-cloud=", "file", "help"))
+    except getopt.GetoptError as exc:
+        usage(1)
+    for opt in opts:
+        if opt[0] == "-h" or opt[0] == "--help":
+            usage(0)
+        elif opt[0] == "-c" or opt[0] == "--os-cloud":
+            cloud = opt[1]
+        elif opt[0] == "-f" or opt[0] == "--file":
+            ofile = opt[1]
 
-        if args:
-                usage(1)
-        conn = openstack.connect(cloud=cloud)
-        mycloud = osCloud()
-        mycloud.regions = list(conn.identity.regions())
-        #mycloud.services = list(conn.identity.services())
-        #mycloud.services = conn.list_services()
-        for svc in conn.service_catalog:
-                mycloud.services.append(osService(svc))
-        #print(conn)
-        print(mycloud)
-        get_openstack_flavors()
+    if args:
+        usage(1)
+    conn = openstack.connect(cloud=cloud)
+    mycloud = osCloud()
+    mycloud.regions = list(conn.identity.regions())
+    #mycloud.services = list(conn.identity.services())
+    #mycloud.services = conn.list_services()
+    for svc in conn.service_catalog:
+        mycloud.services.append(osService(svc))
+    #print(conn)
+    print(mycloud)
+    get_openstack_flavors()
 
 
 if __name__ == "__main__":
-	main(sys.argv)
+    main(sys.argv)
+
+# vim:fenc=utf-8:ts=4:sw=4:noet:sts=4:ai
