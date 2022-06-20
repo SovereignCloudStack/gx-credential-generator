51cf4b1a7f220fd624531191ba6653647d7ae154
Kurt Garloff <kurt@garloff.de>
2022-06-20 15:41:53 +0200

    Move Gaia-X CPU class out to separate file.
    Not currently used anyway.
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

---
+++ gax-cpu.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+#
+# gax-cpu.py
+# 
+# These classes reflect the Gaia-X view on physical infra
+#
+# (c) Kurt Garloff <garloff@osb-alliance.com>, 3/2022 - 6/2022
+# SPDX-License-Identifier: EPL-2.0
+
+
+class CPU:
+    def __init__(self):
+        # Gaia-X attrs
+        self.numberOfCores = 0
+        self.numberOfThreads = 0
+        self.frequency = 0
+        self.boostFrequency = 0
+        self.cacheSize = 0
+        # This would not be interesting typically
+        self.allowedSocket = ""
+        # Virt. attrs -- not in GaX
+        self.dedicatedCore = false
+        self.dedicatedThread = false
+        self.limitOversubscr = false
+
+class MEM:
+    def __init__(self):
+        self.memGB = 0
+        self.ECC = true
+
+
