a9bf01f0133c1b9a28d6be07deb3555dc997f6bd
Kurt Garloff <kurt@garloff.de>
2023-01-15 17:22:56 +0100

    Some hints from pylint.
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- gx_context.py
+++ gx_context.py
@@ -2,12 +2,13 @@
 # vim: set ts=4 sw=4 et:
 #
 # gx-context.py
-#
-# Some settings for the JSON-LD SDs needed in Gaia-X
-#
-# (c) Kurt Garloff <garloff@osb-alliance.com>, 6/2022
-# SPDX-License-Identifier: EPL-2.0
-#
+"""
+Some settings for the JSON-LD SDs needed in Gaia-X
+
+(c) Kurt Garloff <garloff@osb-alliance.com>, 6/2022
+SPDX-License-Identifier: EPL-2.0
+"""
+
 gxcontext = {"@context":
              ["http://www.w3.org/ns/shacl#",
               "http://www.w3.org/2001/XMLSchema#",
