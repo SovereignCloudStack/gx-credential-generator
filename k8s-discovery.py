1c1c25f01922991e07bf35046b4222cc262a754f
Miñon Jimenez, Raul <raul.minon@tecnalia.com>
2023-05-03 15:06:10 +0200

    Refactor discover

--- k8s-discovery.py
+++ k8s-discovery.py
@@ -352,66 +352,7 @@ class KubeCluster:
 
         print(kube_gaia)
 
-        self.regions = list(conn.identity.regions())
-        # Create per region service catalogs
-        self.regcat = {}
-        for region in self.regions:
-            reg = region.id
-            self.regcat[reg] = []
-            for svc in conn.service_catalog:
-                svccat = osServiceCat(svc, reg)
-                if svccat.ep:
-                    self.regcat[reg].append(svccat)
-            if debug:
-                print(f"#DEBUG: Svc Cat region {reg}: {self.regcat}", file=sys.stderr)
-        # Well-known OpenStack services
-        self.ostacksvc = {}
-        if "project_id" in conn.auth:
-            prj_id = conn.auth["project_id"]
-        else:
-            prj_id = conn.identity.get_project_id()
-        # Iterate over regions
-        for region in self.regions:
-            # Keep list of already handled services to avoid duplicates/aliases
-            handled = []
-            # Dictionary to collect OpenStack services
-            ostacksvc = {}
-            reg = region.id
-            if debug:
-                print(f"#INFO: Creation service catalog for region {reg}", file=sys.stderr)
-            # Iterate over service catalog
-            for svc in self.regcat[reg]:
-                assert svc.ep
-                assert reg == svc.region
-                # Treating those two legacy services as non-OpenStack (just list EPs)
-                if svc.type in [*handled, "compute_legacy", "cloudformation"]:
-                    continue
-                newsvc = None
-                for osClass in OSClasses:
-                    if svc.type in osClass.svcID:
-                        newsvc = osClass(conn, svc.type, svc.name, reg, prj_id, svc.ep)
-                        handled.extend(osClass.svcID)
-                        break
-                if not newsvc:
-                    newsvc = osService(conn, svc.type, svc.name, reg, prj_id, svc.ep)
-                    handled.extend((svc.type, newsvc.stype,))
-                # Only attach if conn is non-empty
-                if newsvc.conn:
-                    ostacksvc[newsvc.stype] = newsvc
-                    if debug:
-                        print(f"#DEBUG: Region {reg} added OS Svc {newsvc}" % (reg, newsvc), file=sys.stderr)
-                    svc.consumed = True
-                elif debug:
-                    print(f"#DEBUG: Region {reg} with service {newsvc} without connection", file=sys.stderr)
-            # Handle remaining services that are listed
-            for svc in self.regcat[reg]:
-                if not svc.consumed and svc.type not in ostacksvc:
-                    ostacksvc[svc.type] = nonOSService(svc.type, svc.name, svc.ep.replace(prj_id, "${OS_PROJECT_ID}"))
-                    if debug:
-                        print(f"#DEBUG: Region {reg} added Non-OS {ostacksvc[svc.type]}", file=sys.stderr)
-                    svc.consumed = True
-            self.ostacksvc[reg] = ostacksvc
-        # TODO: Iterate over non-consumed services (global, non-region specifc)
+
 
     def values(self):
         "dict representing stored data"
