8938421b6caadc5e37199ecac06f5e97ac6eaa51
Miñon Jimenez, Raul <raul.minon@tecnalia.com>
2023-05-03 18:24:07 +0200

    Starting with pods

--- k8s-discovery.py
+++ k8s-discovery.py
@@ -7,16 +7,14 @@ Talk to OpenStack APIs to discover environment
 Save discovered information in classes that reflect G-X attributes
 These can then be dumped as YAML or as Gaia-X JSON-LD Self-Description.
 
-(c) Kurt Garloff <garloff@osb-alliance.com>, 3/2022 - 6/2022
+(c) Tecnalia <garloff@osb-alliance.com>, 3/2022 - 6/2022
 (c) Mathias Fechner <fechner@osism.tech>, 3/2022
 SPDX-License-Identifier: EPL-2.0
 """
 
-import sys
 import os
-import getopt
-import json
-import yaml
+import sys
+
 # import openstack
 from kubernetes import client, config
 
@@ -49,284 +47,17 @@ class KubeNode():
         self.capacity = capacity
 
 
-# def valtype(val, typ='xsd:string'):
-#     "Wrapper to return dict pairs with @value and @type as needed in JSON-LD"
-#     return {'@value': val, '@type': typ}
-
-
-# def appenddicts(dct1, *kwd):
-#     "Return dict d1 with items from kwd added"
-#     dct = dct1
-#     for k in kwd:
-#         dct.update(k)
-#     return dct
-
-
-# def versinfo(connprox, stype, region):
-#     "Get list of supported versions and microversions from OS service"
-#     vrdata = connprox.get_all_version_data(region_name=region)
-#     for reg in vrdata:
-#         try:
-#             vdata = vrdata[reg]["public"]
-#             break
-#         except AttributeError:
-#             pass
-#     for keys in vdata:
-#         versdata = vdata[keys]
-#         if keys == stype:
-#             break
-#     vinfo = list(map(lambda x: {'version': x.version, 'url': x.url,
-#                                 'status': x.status,
-#                                 'min_microversion': x.min_microversion,
-#                                 'max_microversion': x.max_microversion},
-#                      filter(lambda x: x.max_microversion, versdata)))
-#     vinfo.extend(list(map(lambda x: {'version': x.version, 'url': x.url, 'status': x.status},
-#                           filter(lambda x: not x.max_microversion, versdata))))
-#     return vinfo
-
-
-# class osServiceCat:
-#     "OpenStack service catalog"
-#
-#     def __init__(self, dct, regfilter):
-#         self.id = dct["id"]
-#         self.region = regfilter
-#         self.ep = None
-#         # interface: we only look at public
-#         for ept in dct["endpoints"]:
-#             if ept["interface"] == "public" and (not regfilter or ept["region_id"] == regfilter):
-#                 self.ep = ept["url"]
-#         self.type = dct["type"]
-#         self.name = dct["name"]
-#         self.consumed = False
-#
-#     def __str__(self):
-#         "textual representation"
-#         return f"{self.type}|{self.name}|{self.ep}"
-
-    # def __repr__(self):
-    #     return str(self)
-
-
-# class osService:
-#     "A generic openStack service, with a proxy connection object from SDK"
-#
-#     def __init__(self, conn, stype, name, region, prj_id, ept, quiet=True):
-#         "c'tor for the OpenStack service to be caled by subclasses"
-#         self.fulltype = stype
-#         if stype[-2] == 'v' and stype[-1].isnumeric():
-#             stype = stype[:-2]
-#         stype = stype.replace('-', '_')
-#         self.stype = stype
-#         self.region = region
-#         self.conn = None
-#         self.apiver = None
-#         self.ep = None
-#         self.versdata = None
-#         self.extensions = None
-#         self.azs = None
-#         try:
-#             if debug:
-#                 print(f"#INFO: Creating Conn for {stype}:{name}", file=sys.stderr)
-#             self.conn = conn.__getattribute__(stype)
-#             self.conn.service_name = name
-#             self.conn.region_name = region
-#             self.apiver = self.conn.get_api_major_version()
-#         except Exception as exc:
-#             if not quiet:
-#                 print(f"#ERROR: No service proxy of type {stype} in SDK.\n{exc}", file=sys.stderr)
-#             # return
-#         if self.conn is None or (self.conn.version is None and self.apiver is None):
-#             print(f"#WARNING: Skipping over {stype} b/c it advertizes no version", file=sys.stderr)
-#             self.conn = None
-#             return
-#         try:
-#             self.ep = self.conn.get_endpoint().replace(prj_id, "${OS_PROJECT_ID}")
-#         except Exception:  # as exc:
-#             if stype == "identity":
-#                 self.ep = conn.auth["auth_url"]
-#             else:
-#                 if self.conn:
-#                     print(f"#ERROR: No endpoint found for service {self.stype} in region {region}",
-#                           file=sys.stderr)
-#                     # raise exc
-#                     self.conn = None
-#                     return
-#         self.versdata = versinfo(self.conn, self.fulltype, region)
-#         try:
-#             # self.extensions = list(map(lambda x: x.name, self.conn.extensions()))
-#             self.extensions = list(map(lambda x: x.alias, self.conn.extensions()))
-#         except Exception as exc:
-#             if not quiet:
-#                 print(f"#WARNING: Service {self.fulltype} does not support getting extensions.\n{exc}",
-#                       file=sys.stderr)
-#         try:
-#             self.azs = list(filter(lambda x: x.state['available'] is True, self.conn.availability_zones()))
-#         except Exception:
-#             try:
-#                 self.azs = list(filter(lambda x: x.state == 'available', self.conn.availability_zones()))
-#             except Exception as exc:
-#                 if not quiet:
-#                     print(f"#WARNING: Service {self.fulltype} does not support getting AZs.\n{exc}",
-#                           file=sys.stderr)
-#         # FIXME: We are not using ept, is that right?
-
-    # def values(self):
-    #     "return dict representing stored values"
-    #     dct = {self.stype: {"name": self.conn.service_name,
-    #                         "endpoint": self.ep,
-    #                         # "region": self.region,
-    #                         "versions": self.versdata}}
-    #     if self.extensions:
-    #         dct[self.stype]["extensions"] = self.extensions
-    #     if self.azs:
-    #         # dct[self.stype]["availability_zones"] = list(map(lambda x: x.name, self.azs))
-    #         dct[self.stype]["availability_zones"] = [x.name for x in self.azs]
-    #     return dct
-    #
-    # def __str__(self):
-    #     "textual representation"
-    #     return str(self.values())
-
-
-# class osFlavor:
-#     "Abstraction for flavors"
-#
-#     def __init__(self, flv):
-#         self.name = flv['name']
-#         # Note: cpuType, cpuGeneration, diskType are MR34 ideas,
-#         # slightly different from and less comprehensive than
-#         # the abstraction in SCS flavor spec. Convert later.
-#         self.cpuType = ""
-#         self.cpuGeneration = ""
-#         self.numberOfvCPUs = flv['vcpus']
-#         self.ramSize = flv['ram']  # MiB
-#         self.diskSize = flv['disk']  # GB
-#         self.diskType = ""
-#
-#     def values(self):
-#         ydct = dict(name=self.name,
-#                     numberOfvCPUs=self.numberOfvCPUs,
-#                     ramSize=dict(Value=self.ramSize / 1024, Unit='GiB'))
-#         if self.diskSize:
-#             ydct['diskSize'] = dict(Value=self.diskSize, Unit='GB')
-#         # TODO: cpuType, cpuGen, diskType output
-#         return ydct
-
-
-# class osCompute(osService):
-#     "Abstraction for compute service (nova)"
-#     svcID = ("compute", "computev2", "compute_legacy")
-#
-#     # def __init__(self, conn, region="RegionOne"):
-#     def __init__(self, conn, stype, name, region, prj_id, ept):
-#         super().__init__(conn, stype, name, region, prj_id, ept, False)
-#         self.flavors = self.get_openstack_flavors()
-#
-#     def values(self):
-#         dct = super().values()
-#         dct[self.stype]["flavors"] = self.flavors
-#         return dct
-#
-#     def get_openstack_flavors(self):
-#         """Use OpenStack conn (global var conn) to get flavor list from
-#            compute service. Populate flavor list."""
-#         flvs = []
-#         for flv_id in self.conn.flavors(id):
-#             flvs.append(osFlavor(flv_id).values())
-#             # TODO:
-#             # (a) parse extra specs if any
-#             # (b) parse SCS flavor names
-#         self.flavors = flvs
-#         return flvs
-
-
-class azInfo:
-    "Convert zoneXxx dict into class with Xxx attributes for availability zones"
-
-    def __init__(self, dct):
-        for key in dct:
-            if key[:4] == "zone":
-                newkey = key[4].lower() + key[5:]
-            else:
-                newkey = key
-            self.__setattr__(newkey, dct[key])
-
-
-# class osVolume(osService):
-#     "osService specialization for volumes (cinderv3)"
-#     svcID = ("volumev3", "volumev2", "volume")
-#
-#     def __init__(self, conn, stype, name, region, prj_id, ept):
-#         super().__init__(conn, stype, name, region, prj_id, ept, True)
-#         self.voltypes = list(self.conn.types())
-#         # TODO: Fixup azs and exts
-#         req = self.conn.request('/extensions', 'GET')
-#         ext = json.loads(req.text)["extensions"]
-#         self.extensions = list(map(lambda x: x["alias"], ext))
-#         req = self.conn.request('/os-availability-zone', 'GET')
-#         azs = json.loads(req.text)["availabilityZoneInfo"]
-#         # self.azs = list(map(lambda x: azInfo(x), filter(lambda x: x["zoneState"]["available"] is True, azs)))
-#         self.azs = [azInfo(x) for x in azs if x["zoneState"]["available"]]
-#
-#     def values(self):
-#         dct = super().values()
-#         # dct[self.stype]["volume_types"] = list(map(
-#         #     lambda x: {"name": x.name, "public": x.is_public}, self.voltypes))
-#         dct[self.stype]["volume_types"] = [{"name": x.name, "public": x.is_public} for x in self.voltypes]
-#         return dct
-
-
-# class osLoadBalancer(osService):
-#     "osService specialization for load balancers"
-#     svcID = ("load_balancer", "load-balancer")
-#
-#     def __init__(self, conn, stype, name, region, prj_id, ept):
-#         super().__init__(conn, stype, name, region, prj_id, ept, True)
-#         try:
-#             self.flavors = self.conn.flavors()
-#             self.flavors = list(map(lambda x: {"name": x.name, "description": x.description},
-#                                     filter(lambda x: x.is_enabled, self.flavors)))
-#         except Exception as exc:
-#             print(exc, file=sys.stderr)
-#             self.flavors = None
-#
-#     def values(self):
-#         dct = super().values()
-#         if self.flavors:
-#             dct[self.stype]["flavors"] = self.flavors
-#         return dct
-
-
-# TODO: List of public images with properties
-
-
-# TODO: Network class needs more detection; have AZs for routers and nets, endpoint not per region
-# class osNetwork(osService):
-#     "osService specialization for network"
-#     svcID = ("network")
-#
-#     def __init__(self, conn, stype, name, region, prj_id, ept):
-#         super().__init__(conn, stype, name, region, prj_id, ept, True)
-#         self.ep = ept
-#     # TODO: Fixup AZs
-
-
-# Known-classes
-# OSClasses = [osCompute, osVolume, osLoadBalancer, osNetwork]
-
-
-# class nonOSService:
-#     "Non-OpenStack services listed in the service catalogue"
-#
-#     def __init__(self, stype, name, url):
-#         self.stype = stype
-#         self.name = name
-#         self.ep = url
-#
-#     def values(self):
-#         "return dict with stored values"
-#         return {self.stype: {"name": self.name, "endpoint": self.ep}}
+class KubePod:
+    def __init__(self, host_ip, pod_i_ps, phase, start_time, qos_class, image, image_id, name, namespace):
+        self.host_ip = host_ip
+        self.pod_i_ps = pod_i_ps
+        self.phase = phase
+        self.start_time = start_time
+        self.qos_class = qos_class
+        self.image = image
+        self.image_id = image_id
+        self.name = name
+        self.namespace = namespace
 
 
 class KubeCluster:
@@ -358,44 +89,26 @@ class KubeCluster:
                 elif address.type == 'InternalIP':
                     internal_ip = address.address
             self.kube_nodes.append(KubeNode(hostname, internal_ip, capacity))
+        pods = conn.list_pod_for_all_namespaces(watch=False)
+        self.kube_pods = []
+        for pod in pods.items:
+            print("%s\t%s\t%s" % (pod.status.pod_ip, pod.metadata.namespace, pod.metadata.name))
+            host_ip = pod.status.host_ip
+            pod_i_ps = pod.status.pod_i_ps
+            phase = pod.status.phase
+            start_time = pod.status.start_time
+            qos_class = pod.status.qos_class
+            image = pod.status.container_statuses[0].image
+            image_id = pod.status.container_statuses[0].image_id
+            name = pod.metadata.name
+            namespace = pod.metadata.namespace
+            self.kube_pods.append(
+                KubePod(host_ip, pod_i_ps, phase, start_time, qos_class, image, image_id, name, namespace))
+
+        # spec.
+        # for service in services
 
         print(kube_gaia)
-
-    # def values(self):
-    #     "dict representing stored data"
-    #     inner = {"regions": list(map(lambda x: x.id, self.regions))}
-    #     if outjson:
-    #         inner["auth_url"] = valtype(self.auth["auth_url"], "xsd:anyURI")
-    #     else:
-    #         inner["auth_url"] = self.auth["auth_url"]
-    #     for reg, ostacksvc in self.ostacksvc.items():
-    #         inner[reg] = {}
-    #         for svckey in ostacksvc:
-    #             svc = ostacksvc[svckey]
-    #             inner[reg] = appenddicts(inner[reg], svc.values())
-    #             if outjson:
-    #                 inner[reg][svc.stype]["endpoint"] = valtype(svc.ep, "xsd:anyURI")
-    #     return inner
-
-    # def __str__(self):
-    #     # print(self.values())
-    #     if outjson:
-    #         return json.dumps({"OpenStackService": self.values()}, indent=indent)
-    #     return yaml.dump({"openstack": self.values()}, default_flow_style=False)
-
-
-# def usage(err=1):
-#     "output help"
-#     print("Usage: openstack-discovery.py [options]", file=sys.stderr)
-#     print("Options: -j/--json:   output GX JSON-LD instead of YAML")
-#     print("         -J/--json-compact:   output compact GX JSON-LD instead of YAML")
-#     print("         -f FILE/--file=FILE: write output to FILE (default: stdout)")
-#     print("         -c CLOUD/--os-cloud=CLOUD: use OpenStack cloud CLOUD (default: $OS_CLOUD)")
-#     print("         -f FILE/--file=FILE: write output to FILE (default: stdout)")
-#     print("         -t TMO/--timeout=TMO: Timeout for the API connections/requests")
-#     if not cloud:
-#         print("You need to have OS_CLOUD set or pass --os-cloud=CLOUD.", file=sys.stderr)
-#     sys.exit(err)
 
 
 def kubeconn():
