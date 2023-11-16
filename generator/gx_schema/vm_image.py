ca67aa42ef60ccae76357d981e8bc66d5ba5fa85
Anja Strunk <anja.strunk@cloudandheat.com>
2023-11-16 13:33:04 +0100

    Add auto-generated pyhton classes for GX Credential Schema
    Signed-off-by: Anja Strunk <anja.strunk@cloudandheat.com>

---
+++ generator/gx_schema/vm_image.py
@@ -0,0 +1,17 @@
+class VmImage():
+
+    def __init__(self,
+                 fileSize: int,
+                 operatingSystem: str,
+                 diskFormat: str,
+                 hypervisorType: str,
+                 firmwareType: str,
+                 hwRngTypeOfImage: str,
+                 watchDogAction: strl,
+                 cpuReq: CPU) -> None:
+        pass
+
+
+
+
+
