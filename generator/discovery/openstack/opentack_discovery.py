c842b9c7926a5f50a5497a06f8f319d8e7dc0e6f
Anja Strunk <anja.strunk@cloudandheat.com>
2024-01-05 15:01:01 +0100

    Support siganure, finish JSON-LD serialization, refactoring
    Signed-off-by: Anja Strunk <anja.strunk@cloudandheat.com>

--- generator/discovery/openstack/opentack_discovery.py
+++ generator/discovery/openstack/opentack_discovery.py
@@ -1,18 +1,14 @@
-import json
-
-import openstack
-from openstack.connection import Connection
-from typing import Dict
-import sys
-
-from uuid import uuid4
 import generator.common.json_ld as json_ld
-from generator.common.gx_schema import VMImage
 
 from generator.discovery.openstack.vm_images_discovery import VmDiscovery
 
+from openstack.connection import Connection
+
+from typing import Dict
+
+
 class OsCloud:
-    "Abstraction for openStack cloud with all its services"
+    "Abstraction for openStack cloud with all its services."
     def __init__(self, conn: Connection, config: Dict) -> None:
         # import copy
         self.conn = conn
@@ -21,6 +17,11 @@ class OsCloud:
         self.config = config
 
     def discover_properties(self) -> dict:
+        """
+        Discover all attributes of OS Cloud.
+
+        @return: all attributes as dict
+        """
         props = json_ld.get_json_ld_context()
         props['@graph'] = []
 
