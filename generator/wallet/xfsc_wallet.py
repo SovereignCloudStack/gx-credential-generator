ec334115610a080d3cd84f332398c339dcf430dd
Anja Strunk <anja.strunk@cloudandheat.com>
2024-02-29 11:33:18 +0100

    Refactoring
    Signed-off-by: Anja Strunk <anja.strunk@cloudandheat.com>

--- generator/wallet/xfsc_wallet.py
+++
@@ -1,8 +0,0 @@
-from generator.wallet.wallet import WalletConnector
-
-
-class XFSCWallet(WalletConnector):
-    """
-    Abstraction XFSC wallet, called Organization Credential Manager.
-    See https://projects.eclipse.org/projects/technology.xfsc
-    """
