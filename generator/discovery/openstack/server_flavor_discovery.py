3667c3cd4e286ab257f2cc5feb49bdf31ca05e87
Matthias Büchse <matthias.buechse@cloudandheat.com>
2024-04-09 15:45:47 +0200

    incorporate scs module for flavor-name parsing
    Signed-off-by: Matthias Büchse <matthias.buechse@cloudandheat.com>

--- generator/discovery/openstack/server_flavor_discovery.py
+++ generator/discovery/openstack/server_flavor_discovery.py
@@ -5,7 +5,7 @@ SPDX-License-Identifier: EPL-2.0
 """
 
 import re
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 from openstack.compute.v2.flavor import Flavor as OS_Flavor
 from openstack.connection import Connection
@@ -25,6 +25,16 @@ from generator.common.gx_schema import (
 )
 from generator.common.gx_schema import ServerFlavor as GX_Flavor
 from generator.common.json_ld import JsonLdObject
+from generator.vendor.flavor_names import Flavorname, parser_v3
+
+
+# map SCS hypervisor names to corresponding GX type and config key
+HYPERVISOR_LOOKUP = {
+    "kvm": ("KVM", const.CONFIG_HV_KVM),
+    "xen": ("Xen", const.CONFIG_HV_XEN,
+    "vmw": ("KVM", const.CONFIG_HV_VMW),  # FIXME is this correct?
+    "hyv": ("Hyper-V", const.CONFIG_HV_HYV),
+}
 
 
 class ServerFlavorDiscovery:
@@ -54,12 +64,16 @@ class ServerFlavorDiscovery:
         @rtype ServerFlavor
         """
 
+        try:
+            flavorname = parser_v3(os_flavor.name)
+        except ValueError:
+            flavorname = None
         # Initialize Gaia-X Server Flavor
-        disks = self._get_disks(os_flavor)
+        disks = self._get_disks(os_flavor, flavorname)
         gx_flavor = GX_Flavor(
             # name=os_flavor.name,
-            cpu=self._get_cpu(os_flavor),
-            ram=self._get_ram(os_flavor),
+            cpu=self._get_cpu(os_flavor, flavorname),
+            ram=self._get_ram(os_flavor, flavorname),
             bootVolume=disks[0],
         )
 
@@ -67,12 +81,12 @@ class ServerFlavorDiscovery:
             gx_flavor.additionalVolume = additionalVolume = disks[1:]
 
         # Discover optional attributes
-        self._parse_optional_flavor_properties(os_flavor, gx_flavor)
+        self._parse_optional_flavor_properties(flavorname, gx_flavor)
         self._add_description(os_flavor, gx_flavor)
 
         return gx_flavor
 
-    def _get_cpu(self, os_flavor: OS_Flavor) -> CPU:
+    def _get_cpu(self, os_flavor: OS_Flavor, flavorname: Optional[Flavorname]) -> CPU:
         """
         Return Gaia-X compliance CPU specification of given OpenStack flavor.
         @param os_flavor: OpenStack flavor
@@ -81,30 +95,16 @@ class ServerFlavorDiscovery:
         @rtype CPU
         """
         cpu = CPU(cpuArchitecture=CpuArch.other, numberOfCores=os_flavor.vcpus)
-        if os_flavor.name.startswith("SCS-"):
-            # parse SCS flavor name, second part contains CPU properties
-            parts = os_flavor.name.split("-")
-            if len(parts) >= 2 and re.search(r"\d[V,T,C,L]$", parts[1]):
-                cpu_suffix = parts[1][-1]
-                cpu.numberOfCores = int(parts[1][-2])
-                if cpu_suffix.endswith("i"):
-                    cpu_suffix = parts[1][-2]
-                    cpu.numberOfCores = int(parts[1][-3])
-                if cpu_suffix == "C":
-                    cpu.smtEnabled = False
-                    cpu.defaultOversubscriptionRatio = 1
-                elif cpu_suffix.endswith("T"):
-                    cpu.smtEnabled = True
-                    cpu.defaultOversubscriptionRatio = 1
-                elif cpu_suffix.endswith("V"):
-                    cpu.smtEnabled = True
-                    cpu.defaultOversubscriptionRatio = 5
-                elif cpu_suffix.endswith("L"):
-                    cpu.smtEnabled = True
-                    cpu.defaultOversubscriptionRatio = 16
+        if flavorname:
+            cpu.smtEnabled = flavorname.cpuram.cputype != "C"  # FIXME this is unclear to me
+            cpu.defaultOversubscriptionRatio = 1
+            if flavorname.cpuram.cputype == "V":
+                cpu.defaultOversubscriptionRatio = 5
+            elif flavorname.cpuram.cputype == "L":
+                cpu.defaultOversubscriptionRatio = 16
         return cpu
 
-    def _get_ram(self, os_flavor: OS_Flavor) -> Memory:
+    def _get_ram(self, os_flavor: OS_Flavor, flavorname: Optional[Flavorname]) -> Memory:
         """
         Return Gaia-X RAM definition specified in given OpenStack flavor.
         @param os_flavor: OpenStack Flavor
@@ -114,30 +114,13 @@ class ServerFlavorDiscovery:
         """
         size = MemorySize(value=float(os_flavor.ram), unit=const.UNIT_MB)
         mem = Memory(memorySize=size)
-        if os_flavor.name.startswith("SCS-"):
-            # parse SCS flavor name
-            parts = os_flavor.name.split("-")
-            if len(parts) >= 3:
-                mem_cap = parts[2]
-                if mem_cap.endswith("ou"):
-                    # this order of memory suffixes is not allows
-                    return mem
-                elif mem_cap.endswith("uo"):
-                    mem.eccEnabled = True
-                    mem.defaultOversubscriptionRatio = 2
-                    mem.eccEnabled = True
-                    mem.memorySize.value = float(mem_cap[0:-2])
-                elif mem_cap.endswith("u"):
-                    mem.eccEnabled = True
-                    mem.memorySize.value = float(mem_cap[0:-1])
-                elif mem_cap.endswith("o"):
-                    mem.defaultOversubscriptionRatio = 2
-                    mem.memorySize.value = float(mem_cap[0:-1])
-                else:
-                    mem.memorySize.value = float(mem_cap)
+        if flavorname:
+            mem.eccEnabled = not flavorname.cpuram.raminsecure
+            if flavorname.cpuram.ramoversubscribed:
+                mem.defaultOversubscriptionRatio = 2
         return mem
 
-    def _get_disks(self, os_flavor: OS_Flavor) -> List[Disk]:
+    def _get_disks(self, os_flavor: OS_Flavor, flavorname: Optional[Flavorname]) -> List[Disk]:
         """
         Return all disk specification found in given Openstack flavor.
         @param os_flavor: Openstack flavor specification
@@ -145,25 +128,26 @@ class ServerFlavorDiscovery:
         @return: list of Gaia-X disk specifications
         """
         disks = []
-        if os_flavor.name.startswith("SCS-"):
-            # parse SCS flavor name
-            parts = os_flavor.name.split("-")
-            if len(parts) < 4:
-                # either no SCS compliant flavor name or disk spec missing
+        if flavorname and flavorname.disk:
+            disk_type = DiskType.other.text
+            disk_bus_types = DiskType.other.text
+            if flavorname.disk.disktype == "n":
+                disk_type = "shared network storage"
+            elif flavorname.disk.disktype == "h":
+                disk_type = "local HDD"
+            elif flavorname.disk.disktype == "s":
+                disk_type = "local SSD"
+            elif flavorname.disk.disktype == "p":
+                disk_type = "local HDD"
+                disk_bus_types = "NVMe"
+            for i in range(int(flavorname.disk.nrdisks)):
                 disks.append(
-                    Disk(diskSize=MemorySize(value=os_flavor.disk, unit=const.UNIT_GB))
-                )
-            else:
-                # SCS compliant flavor name with spec of disk capabilities
-                number, size, disk_type, disk_bus_types = self._get_disk_caps(parts[3])
-                for i in range(int(number)):
-                    disks.append(
-                        Disk(
-                            diskSize=MemorySize(value=size, unit=const.UNIT_GB),
-                            diskType=disk_type,
-                            diskBusType=disk_bus_types,
-                        )
+                    Disk(
+                        diskSize=MemorySize(value=flavorname.disk.disksize, unit=const.UNIT_GB),
+                        diskType=disk_type,
+                        diskBusType=disk_bus_types,
                     )
+                )
         else:
             # no SCS compliant flavor name. Use disk property to set disk spec
             disks.append(
@@ -172,45 +156,8 @@ class ServerFlavorDiscovery:
 
         return disks
 
-    def _get_disk_caps(self, disk_caps: str) -> Tuple[int, int, DiskType, DiskBusType]:
-        """
-        Extracts disk capabilities encoded in flavor name according to SCS flavor naming standard.
-        @param disk_caps: Disk capabilities encoded as string according to SCS flavor naming standard.
-        @type disk_caps: str
-        @return:
-        """
-        disk_type = DiskType.other.text
-        disk_bus_types = DiskType.other.text
-        number = 1
-
-        # parse disk type and disk bus type
-        if re.search(r"\d+$", disk_caps):
-            # no disk type set
-            size = disk_caps[0 : len(disk_caps)]
-        else:
-            # disk type set
-            size = disk_caps[0 : len(disk_caps) - 1]
-            # parse disk type and disk bus type
-            if disk_caps.endswith("n"):
-                disk_type = "shared network storage"
-            elif disk_caps.endswith("h"):
-                disk_type = "local HDD"
-            elif disk_caps.endswith("s"):
-                disk_type = "local SSD"
-            elif disk_caps.endswith("p"):
-                disk_type = "local HDD"
-                disk_bus_types = "NVMe"
-        try:
-            number, size = size.split("x")
-        except ValueError:
-            if len(size) == 0:
-                # no size set
-                size = 0
-
-        return int(number), int(size), DiskType(disk_type), DiskBusType(disk_bus_types)
-
     def _parse_optional_flavor_properties(
-        self, os_flavor: OS_Flavor, gx_flavor: GX_Flavor
+        self, flavorname: Optional[Flavorname], gx_flavor: GX_Flavor
     ):
         """Parse and return optional flavor properties, such as CPU architecture, CPU frequency or hypervisor.
         @param os_flavor Openstack flavor
@@ -218,92 +165,68 @@ class ServerFlavorDiscovery:
         @param gx_flavor Gaia-X Flavor specification
         @type gx_flavor ServerFlavor
         """
-        parts = os_flavor.name.split("_")
-        for suffix in parts:
-            if suffix.startswith("SCS"):
-                continue
-            # parse hypervisor
-            elif suffix == "kvm":
-                gx_flavor.hypervisor = Hypervisor(
-                    hypervisorType="KVM",
-                    copyrightOwnedBy=self.conf.get_copyright_owner(const.CONFIG_HV_KVM),
-                    license=self.conf.get_license(const.CONFIG_HV_KVM),
-                    resourcePolicy=self.conf.get_resource_policy(const.CONFIG_HV_KVM),
-                )
-            elif suffix == "xen":
-                gx_flavor.hypervisor = Hypervisor(
-                    hypervisorType="Xen",
-                    copyrightOwnedBy=self.conf.get_copyright_owner(const.CONFIG_HV_XEN),
-                    license=self.conf.get_license(const.CONFIG_HV_XEN),
-                    resourcePolicy=self.conf.get_resource_policy(const.CONFIG_HV_XEN),
-                )
-            elif suffix == "vmw":
-                gx_flavor.hypervisor = Hypervisor(
-                    hypervisorType="KVM",
-                    copyrightOwnedBy=self.conf.get_copyright_owner(const.CONFIG_HV_VMW),
-                    license=self.conf.get_license(const.CONFIG_HV_VMW),
-                    resourcePolicy=self.conf.get_resource_policy(const.CONFIG_HV_VMW),
-                )
-            elif suffix == "hyv":
-                gx_flavor.hypervisor = Hypervisor(
-                    hypervisorType="Hyper-V",
-                    copyrightOwnedBy=self.conf.get_copyright_owner(const.CONFIG_HV_HYV),
-                    license=self.conf.get_license(const.CONFIG_HV_HYV),
-                    resourcePolicy=self.conf.get_resource_policy(const.CONFIG_HV_HYV),
-                )
-            # parse hardware assisted virtualization
-            elif suffix == "hwv":
-                gx_flavor.hardwareAssistedVirtualization = True
-            # parse CPU architecture details
-            elif suffix.startswith("i"):
+        if flavorname is None:
+            return
+        if flavorname.hype and flavorname.hype.hype in HYPERVISOR_LOOKUP:
+            hv_type, conf_key = HYPERVISOR_LOOKUP[flavorname.hype.hype]
+            gx_flavor.hypervisor = Hypervisor(
+                hypervisorType=hv_type,
+                copyrightOwnedBy=self.conf.get_copyright_owner(conf_key),
+                license=self.conf.get_license(conf_key),
+                resourcePolicy=self.conf.get_resource_policy(conf_key),
+            )
+        if flavorname.hwvirt and flavorname.hwvirt.hwvirt:
+            gx_flavor.hardwareAssistedVirtualization = True
+        if flavorname.cpubrand:
+            if flavorname.cpubrand.cpuvendor == "i":
                 # Intel x86-64
                 gx_flavor.cpu.cpuArchitecture = CpuArch("x86-64")
                 gx_flavor.cpu.vendor = "Intel"
-                if suffix.startswith("i0"):
+                if flavorname.cpubrand.cpugen == 0:
                     gx_flavor.cpu.generation = "pre Skylake"
-                elif suffix.startswith("i1"):
+                elif flavorname.cpubrand.cpugen == 1:
                     gx_flavor.cpu.generation = "Skylake"
-                elif suffix.startswith("i2"):
+                elif flavorname.cpubrand.cpugen == 2:
                     gx_flavor.cpu.generation = "Cascade Lake"
-                elif suffix.startswith("i3"):
+                elif flavorname.cpubrand.cpugen == 3:
                     gx_flavor.cpu.generation = "Ice Lake"
-                elif suffix.startswith("i4"):
-                    gx_flavor.cpu.generation = "Ice Lake"
-            elif suffix.startswith("z"):
+                elif flavorname.cpubrand.cpugen == 4:
+                    gx_flavor.cpu.generation = "Ice Lake"  # FIXME is this correct? (Sapphire Rapids)
+            elif flavorname.cpubrand.cpuvendor == "z":
                 #  	AMD (Zen) x86-64
                 gx_flavor.cpu.cpuArchitecture = CpuArch("x86-64")
                 gx_flavor.cpu.vendor = "AMD"
-                if suffix.startswith("z0"):
+                if flavorname.cpubrand.cpugen == 0:
                     gx_flavor.cpu.generation = "pre Zen"
-                elif suffix.startswith("z1"):
+                elif flavorname.cpubrand.cpugen == 1:
                     gx_flavor.cpu.generation = "Zen-1 (Naples)"
-                elif suffix.startswith("z2"):
-                    gx_flavor.cpu.generation = "Zen-2 (Rome"
-                elif suffix.startswith("z3"):
+                elif flavorname.cpubrand.cpugen == 2:
+                    gx_flavor.cpu.generation = "Zen-2 (Rome"  # FIXME is this correct? (closing paren)
+                elif flavorname.cpubrand.cpugen == 3:
                     gx_flavor.cpu.generation = "Zen-3 (Milan)"
-                elif suffix.startswith("z4"):
+                elif flavorname.cpubrand.cpugen == 4:
                     gx_flavor.cpu.generation = "Zen-4 (Genoa)"
-            elif suffix.startswith("a"):
+            elif flavorname.cpubrand.cpuvendor == "a":
                 #  	ARM v8+
                 gx_flavor.cpu.cpuArchitecture = CpuArch("AArch-64")
                 gx_flavor.cpu.vendor = "ARM"
-                if suffix.startswith("a0"):
+                if flavorname.cpubrand.cpugen == 0:
                     gx_flavor.cpu.generation = "pre Cortex A76"
-                elif suffix.startswith("a1"):
+                elif flavorname.cpubrand.cpugen == 1:
                     gx_flavor.cpu.generation = "A76/NeoN1 class"
-                elif suffix.startswith("a2"):
+                elif flavorname.cpubrand.cpugen == 2:
                     gx_flavor.cpu.generation = "A78/x1/NeoV1 class"
-                elif suffix.startswith("a3"):
+                elif flavorname.cpubrand.cpugen == 3:
                     gx_flavor.cpu.generation = "A71x/NeoN2 (ARMv9)"
-            elif suffix.startswith("r"):
+            elif flavorname.cpubrand.cpuvendor == "r":
                 #  	RISC-V
                 gx_flavor.cpu.cpuArchitecture = CpuArch("RISC-V")
             # parse frequency
-            if suffix.endswith("hhh"):
+            if suffix.perf == "hhh":
                 gx_flavor.cpu.baseFrequency = Frequency(value=3.75, unit=const.UNIT_GHZ)
-            elif suffix.endswith("hh"):
+            elif suffix.perf == "hh":
                 gx_flavor.cpu.baseFrequency = Frequency(value=3.25, unit=const.UNIT_GHZ)
-            elif suffix.endswith("h"):
+            elif suffix.perf == "h":
                 gx_flavor.cpu.baseFrequency = Frequency(value=2.75, unit=const.UNIT_GHZ)
 
     def _add_description(self, os_flavor: OS_Flavor, gx_flavor: GX_Flavor) -> None:
